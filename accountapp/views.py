7a7d15efba2c4bfd5c02c1d991f6016f500f0a5c
rhgud <kohsmic8@gmail.com>
2021-07-09 12:07:02 +0900

    7.9 test33 commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def hello_world(request):                        ##request
-    return render(request,'accountapp/hello_world.html')
+def hello_world(request):
+    if request.method == "POST":
+        return render(request,'accountapp/hello_world.html',context={'text':'POST METHOD!'})
+
+    else:
+        return render(request,'accountapp/hello_world.html',context={'text':'GET METHOD!'})
