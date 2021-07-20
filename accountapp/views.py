da54e64d70f1f529c762afc018a226dca9983a1c
rhgud <kohsmic8@gmail.com>
2021-07-20 11:35:23 +0900

    7.20 test33 commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import NewModel
 
@@ -38,4 +38,11 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/update.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
 
