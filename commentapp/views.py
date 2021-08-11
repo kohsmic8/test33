690baf9aba93fd47819ee0a848c2b8c053f7f464
rhgud <kohsmic8@gmail.com>
2021-08-11 17:37:53 +0900

    8.11 test33 commit 2

--- commentapp/views.py
+++ commentapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
