8904c66f7c2cb44ccb654d39c375638554664c63
rhgud <kohsmic8@gmail.com>
2021-08-23 09:46:45 +0900

    8.20 test33 commit 3

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,5 @@
+
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -10,13 +12,14 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
-
     template_name = 'projectapp/create.html'
 
     def get_success_url(self):
@@ -29,12 +32,21 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        article_list = Article.objects.filter(project=self.object)
-        return super().get_context_data(object_list=article_list, **kwargs)
+        user = self.request.user
+        project = self.object
 
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+        else:
+            subscription = None
+
+        article_list = Article.objects.filter(project=self.object)
+        return super().get_context_data(object_list=article_list,
+                                        subscription=subscription,
+                                        **kwargs)
 
 class ProjectListView(ListView):
     model = Project
     context_object_name = 'project_list'
-    template_name ='projectapp/list.html'
+    template_name = 'projectapp/list.html'
     paginate_by = 20
