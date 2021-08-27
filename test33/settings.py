cb663c6d24ff240772c28109eb0bd3e90330a65b
rhgud <kohsmic8@gmail.com>
2021-08-27 11:45:06 +0900

    8.27 test33 commit 2

--- test33/settings.py
+++ test33/settings.py
@@ -162,7 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGIN_REDIRECT_URL = reverse_lazy('articleapp:list')
 # reverse_lazy hello world 로 돌아가게 만드는 구문
 
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
