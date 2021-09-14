02fa37b63ffcc27d85865aa10ca1961081b24117
rhgud <kohsmic8@gmail.com>
2021-09-14 11:20:20 +0900

    9.14 test33 commit 1

--- test33/settings/base.py
+++ test33/settings/base.py
@@ -15,32 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-env_list = dict()
-
-local_env =open(os.path.join(BASE_DIR, '.env'), encoding='utf-8')
-
-while True:
-    line = local_env.readline()     ##한줄씩 읽다가 없으면 나온다 break
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')             ## SECRET_KEY=django-insecu = 로 좌 -key / 우 -value
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value              ## key, value 나눈걸 딕셔러리에 추가
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Application definition
@@ -96,16 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'test33.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
