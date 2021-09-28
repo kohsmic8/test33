4ded9f67715cc6a165cd2ecda81dace2f17936c6
rhgud <kohsmic8@gmail.com>
2021-09-28 11:49:52 +0900

    9.28 test33 commit 3

--- test33/settings/deploy.py
+++ test33/settings/deploy.py
@@ -1,24 +1,31 @@
-from .base import *
+# from .base import *
 
-env_list = dict()
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.lstrip().rstrip()
+    file.close()
+    return secret
 
-local_env =open(os.path.join(BASE_DIR, '.env'), encoding='utf-8')
-
-while True:
-    line = local_env.readline()     ##한줄씩 읽다가 없으면 나온다 break
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')             ## SECRET_KEY=django-insecu = 로 좌 -key / 우 -value
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value              ## key, value 나눈걸 딕셔러리에 추가
+# env_list = dict()
+#
+# local_env =open(os.path.join(BASE_DIR, '.env'), encoding='utf-8')
+#
+# while True:
+#     line = local_env.readline()     ##한줄씩 읽다가 없으면 나온다 break
+#     if not line:
+#         break
+#     line = line.replace('\n', '')
+#     start = line.find('=')             ## SECRET_KEY=django-insecu = 로 좌 -key / 우 -value
+#     key = line[:start]
+#     value = line[start+1:]
+#     env_list[key] = value              ## key, value 나눈걸 딕셔러리에 추가
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -33,8 +40,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': '1234',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
